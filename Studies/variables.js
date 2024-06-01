b7465a95b4c5d44504288c88b5081d6d1712fdff
jhernandez504 <jeremy.hernandez504@gmail.com>
2024-06-01 14:07:24 -0500

    studiesproject structure

---
+++ Studies/variables.js
@@ -0,0 +1,15 @@
+/**
+ * VARIABLES:
+ * 
+ * 0: Variables are named containers for data in Javascript. We create variables using declarative keywords.
+ * Variables are named identifiers that can point to values of a particular type. 
+ * They can be a Number, String, Boolean, Array, Object or another datatype.
+ * They are called variables because once they are created, they can be have their value and datatype can be changed.
+ * 
+ * 1: To create a variable we use the keyword, var,
+ * followed by a name (id or alias) for our variable.
+ * 
+ * 2: There are 2 phases of using variables: declaration
+ * and initialization (or assignment). 
+ *
+ */
