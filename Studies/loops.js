e628b0bf3273416f6776cc34b682299b5111f1ea
jhernandez504 <jeremy.hernandez504@gmail.com>
2024-06-23 19:38:52 -0500

    updating loops.js

---
+++ Studies/loops.js
@@ -0,0 +1,17 @@
+/**
+ * LOOPS:
+ * 
+ * Loops are one of the most useful structures in javascript.
+ * They allow us to execute code for a number of times based on a certain condition.
+ * They are essential for executing code repeatedly, efficiently.
+ * 
+ */
+
+// 1. For loop //
+// executers code for a specified number of times
+
+// 1a. structure
+for (initialization; condition; increment/decrement) {
+ //  ^ set counter.  ^condition.  ^increments counter each loop  
+    // code to be executed
+}
